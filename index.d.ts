e0e2e372924c876ea736bac88b6c850fa77f809c
廖凯 <konelove2010@gmail.com>
2016-04-24 14:06:28 +0800

    Add typescript support

---
+++ index.d.ts
@@ -0,0 +1,52 @@
+export declare function easeLinear(t: any);
+export declare function easeQuad(t: any);
+export declare function easeQuadIn(t: any);
+export declare function easeQuadOut(t: any);
+export declare function easeQuadInOut(t: any);
+
+export declare function easeCubic(t: any);
+export declare function easeCubicIn(t: any);
+export declare function easeCubicOut(t: any);
+export declare function easeCubicInOut(t: any);
+
+
+export declare function easePoly(t: any);
+export declare function easePolyIn(t: any);
+export declare function easePolyOut(t: any);
+export declare function easePolyInOut(t: any);
+
+
+export declare function easeSin(t: any);
+export declare function easeSinIn(t: any);
+export declare function easeSinOut(t: any);
+export declare function easeSinInOut(t: any);
+
+
+export declare function easeExp(t: any);
+export declare function easeExpIn(t: any);
+export declare function easeExpOut(t: any);
+export declare function easeExpInOut(t: any);
+
+
+export declare function easeCircle(t: any);
+export declare function easeCircleIn(t: any);
+export declare function easeCircleOut(t: any);
+export declare function easeCircleInOut(t: any);
+
+
+export declare function easeBounce(t: any);
+export declare function easeBounceIn(t: any);
+export declare function easeBounceOut(a: any, q: any);
+export declare function easeBounceInOut(t: any);
+
+
+export declare function easeBack(t: any);
+export declare function easeBackIn(t: any);
+export declare function easeBackOut(t: any);
+export declare function easeBackInOut(t: any);
+
+
+export declare function easeElastic(a: any, p: any);
+export declare function easeElasticIn(a: any, p: any);
+export declare function easeElasticOut(a: any, p: any);
+export declare function easeElasticInOut(a: any, p: any);
